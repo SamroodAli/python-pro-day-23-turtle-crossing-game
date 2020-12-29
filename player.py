0fc45ba0c33c25c41c1e9bf45c8406b0797970f2
SamroodAli <samroodali@live.com>
2020-12-29 17:00:47 +0530

    Add:turtle moving up event listener

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 """Player data using turtle module"""
-from turtle import Turtle
+from turtle import Turtle, onkey
 STARTING_POSITION = (0, -270)
 
 
@@ -12,3 +12,10 @@ class Player(Turtle):
         self.penup()
         self.setheading(90)
         self.goto(STARTING_POSITION)
+        self.event_listener()
+
+    def move_up(self):
+        self.forward(25)
+
+    def event_listener(self):
+        onkey(key="Up", fun=self.move_up)
